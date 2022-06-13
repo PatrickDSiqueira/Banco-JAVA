b7c6b0a25ecdd0afd3a93875eff418fbe834c6c5
Patrick Siqueira <psique.estuda@gmail.com>
2022-06-13 15:51:16 -0300

    Passagemvariaveis escopo global, criacao da funcao creditar e escolhe tipo conta

--- banco/banco.java
+++ banco/banco.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
  * main
  */
 public class banco {
+    public static Scanner ler = new Scanner(System.in);
+    public static String nomeCliente = "classBanco", tipoConta="";
+    public static int numeroContaCorrente, numeroContaPoupanca, opcao = 0;
+    public static double saldoContaCorrente, saldoContaPoupanca;
 public static void main (String[]args) {
-    Scanner ler = new Scanner(System.in);
-    String nomeCliente;
-    int numeroContaCorrente, numeroContaPoupanca, opcao = 0;
-    double saldoContaCorrente, saldoContaPoupanca;
-    
+    incio();
+    menu();
+}
+
+private static void incio() {
     // System.out.println("Entre com o nome do cliente: ");
     nomeCliente = /*ler.nextLine();*/ "Pedro Dutra";
-    // System.out.println("Entre com o número da Conta Corrente: ");
+    System.out.println("Entre com o número da Conta Corrente: ");
     numeroContaCorrente = /*ler.nextInt();*/ 20330;
     // System.out.println("Entre com o saldo inicial da Conta Corrente "+numeroContaCorrente+": ");
     saldoContaCorrente = /*ler.nextDouble();*/ 100.00;
@@ -20,21 +24,16 @@ public static void main (String[]args) {
     numeroContaPoupanca = /*ler.nextInt();*/ 20340;
     // System.out.println("Entre com seu saldo inicial da Conta Poupança "+numeroContaPoupanca+": ");
     saldoContaPoupanca = /*ler.nextDouble();*/ 200.00;
-
-    
-    menu(opcao, ler);
-    ler.close();
-
 }
 
-private static void menu(int opcao, Scanner ler) {
+private static  void menu() {
     System.out.println("-------------------------------------------------- \n Qual operação deseja realizar: \n     1 - Creditar \n     2 - Debitar \n     3 - Transferir \n     4 - Saldo \n     5 - Sair");
     System.out.print("Opção -> ");
     opcao = ler.nextInt();
 
     switch (opcao) {
         case 1:
-            System.out.println("Opção -> "+opcao);
+            creditar();        
             break;
         case 2:
             System.out.println("Opção -> "+opcao);
@@ -51,8 +50,42 @@ private static void menu(int opcao, Scanner ler) {
     
         default:
             System.out.println("Opção inválida");
+            menu();
             break;
     }
     
+}
+
+private static void creditar() {
+    escolheConta();
+    if (tipoConta.equals("Corrente")) {
+        System.out.print("Qual valor a creditar na conta "+tipoConta+" "+numeroContaCorrente+" -> R$ ");
+        saldoContaCorrente += ler.nextDouble();
+        System.out.printf("Saldo atual na Conta Corrente %d -> R$ %.2f\n", numeroContaCorrente, saldoContaCorrente);
+    } else if (tipoConta.equals("Poupança")) {
+        System.out.print("Qual valor a creditar na conta Poupança "+numeroContaPoupanca+" -> R$ ");
+        saldoContaPoupanca += ler.nextDouble();
+        System.out.printf("Saldo atual na Conta Poupança %d -> R$ %.2f\n", numeroContaPoupanca, saldoContaPoupanca);
+    }
+    menu();
+}
+
+private static String escolheConta() {
+    System.out.println("Qual conta:\n    1 - Corrente\n    2 - Poupança");
+    System.out.print(" Opção -> ");
+    opcao = ler.nextInt();
+   switch (opcao) {
+       case 1:
+           tipoConta = "Corrente";
+           break;
+       case 2:
+            tipoConta = "Poupança";
+            break;
+       default:
+           System.out.println("Opção inválida");
+           escolheConta();
+           break;
+   }
+return tipoConta;
 }
 }
